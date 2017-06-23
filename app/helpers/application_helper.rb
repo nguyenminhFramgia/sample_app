6f7593376bdbefc6911f71fd751666c1f6df297e
NguyenVanMinh <nguyen.van.minh@framgia.com>
2017-06-23 16:59:20 +0700

    Add a full_title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def full_title page_title = nil
+    base_title = t "application_helper.base_title"
+    page_title ? page_title + " | " + base_title : page_title
+  end
 end
