f22817519fd338012e8a4f2fa0ce17671ab70d83
NguyenVanMinh <nguyen.van.minh@framgia.com>
2017-07-11 15:26:57 +0700

    Add password reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get "password_resets/new"
+  get "password_resets/edit"
+
   root "static_pages#home"
 
   get "/help", to: "static_pages#help"
@@ -14,4 +17,6 @@ Rails.application.routes.draw do
   resources :users
 
   resources :account_activations, only: :edit
+
+  resources :password_resets, except: [:index, :show, :destroy]
 end
